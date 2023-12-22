f96395eba0322abbf1aa63fd5f13c75011bf515c
Vitaliy-Kuzmenko <bitlers@yandex.ru>
2023-12-22 21:42:53 +0300

    Подсчёт введённых чисел

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const cardNumber = document.getElementById("cardNumber");
 const btnCard = document.getElementById("btnCard");
 const lotoDraw = document.getElementById('loto-draw')
+const printNumbersEntered = document.getElementById('printNumbersEntered')
+let numbersEntered = 0
 const arrBtn = [];
 const userNumbers = [];
 const winnerNumbers = [1,2,3,4,5]
@@ -14,7 +16,23 @@ const moneyWins = {
     4: 'You wins 100 dollars',
     5: 'You wins JECKPOT'
 }
+
+// Подсчёт введённых чисел
+const checkInputNumbers = ()=>{
+    if(userNumbers.length -1 == 5){
+        printNumbersEntered.innerHTML = `${userNumbers.length} БОЛЬШЕ НЕЛЬЗЯ!`
+        console.log('stop')
+    } else {
+        numbersEntered +=1
+        printNumbersEntered.innerHTML = `${userNumbers.length}`
+        console.log('ещё')
+    }console.log('ещё')
+}
+
+
 //Настройка кнопок с цифрами
+
+
 for (let i = 1; i <= 36; i++) {
     const newButton = document.createElement("button");
     newButton.className = "button-style";
@@ -25,35 +43,24 @@ for (let i = 1; i <= 36; i++) {
 
     cardNumber.appendChild(newButton);
     arrBtn.push(newButton);
-
 // объявляю событие клик для каждой из кнопок
-    newButton.addEventListener("click", myFun);
-
-    function myFun() {
+    const btnSetingsFn = () => {
         arrBtn[i - 1].style.backgroundColor = "red";
         userNumbers.push(Number(arrBtn[i - 1].value));
-        newButton.removeEventListener("click", myFun); // снял событие клик с кнопки на котолрую нажали
-        checkClickNumbers()
+        newButton.removeEventListener("click", btnSetingsFn); // снял событие клик с кнопки на котолрую нажали  
+        checkInputNumbers() 
 }
-}
-
-// подсчёт кол-ва нажатых кнопок
-const checkClickNumbers = ()=> {
-    if (userNumbers.length === 5){
-        arrBtn.forEach((element)=>{
-            element.removeEventListener('click', myFun)
-        })
-    }
+    newButton.addEventListener("click", btnSetingsFn);
 }
 
-console.log(userNumbers.length)
+
+
 
 
 const sendUserNumbers = document.getElementById("sendUserNumbers"); //кнопка отправить
 const printUserNumbers = document.getElementById("printUserNumbers"); // кнопка проверить
 
-sendUserNumbers.addEventListener("click", sendNumFun);
-function sendNumFun() {
+const sendNumFun = ()=> {
     //сортировка по возрастанию
     userNumbers.sort(function (a, b) {
         return a - b;
@@ -62,6 +69,9 @@ function sendNumFun() {
     const strUserNumbers = userNumbers.join("-"); //вывод в строку с разделителем
     printUserNumbers.innerHTML = strUserNumbers;
 }
+
+
+sendUserNumbers.addEventListener("click", sendNumFun);
 
 const checkWinsBtn = document.getElementById("checkWinsBtn");
 const printUserWins = document.getElementById("printUserWins");
